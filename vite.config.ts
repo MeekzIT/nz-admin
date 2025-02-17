b20c2ee7e8c72f2f808847b6126c00ab905dc41c
gevorg <gevorg@branchdev.io>
2025-02-17 17:16:45 +0400

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "185.170.196.1", // Set the host to your desired IP
+    port: 3000, // Specify the port
+    open: true, // This will open the browser automatically
+  },
+});
