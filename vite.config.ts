c5a3273c4020b4f6ea4557fed82ec1fb17e0ba57
gevorg <gevorg@branchdev.io>
2025-02-26 14:58:21 +0400

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0", // Allow access from any IP
-    port: 3001, // Specify the port
+    port: 3001, // Your development server port
     open: true,
+    strictPort: true, // Ensures Vite only runs on the specified port
+    cors: true, // Allows cross-origin requests
+    allowedHosts: ["admin.nurazyanconstruction.am"], // Allow requests from your domain
   },
 });
