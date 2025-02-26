7689f2303e4efc582817753057ab872647882974
gevorg <gevorg@branchdev.io>
2025-02-26 14:47:59 +0400

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    // host: "185.170.196.1", // Set the host to your desired IP
-    port: 3000, // Specify the port
+    host: "185.170.196.1", // Set the host to your desired IP
+    port: 3001, // Specify the port
     open: true, // This will open the browser automatically
   },
 });
